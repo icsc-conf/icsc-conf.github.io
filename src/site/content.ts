0a415af715b2b543c3a58caaedd12f4280d79b70
crahal <charlierahal@gmail.com>
2026-08-30 14:48:00 +0100

    Update poster presentation with revised presenter details

--- src/site/content.ts
+++ src/site/content.ts
@@ -175,7 +175,7 @@ export const posterPresentations = [
   {
     title:
       "Making Low-Visibility Policies Discoverable: A Document-Embedding Approach to Inclusive Cross-Boundary Policy Comparison",
-    presenter: "Yujing Zhou and Chenxi Lin",
+    presenter: "Yujing Zhou",
     href: "https://openreview.net/forum?id=Ndk4RFNtsL"
   },
   {
